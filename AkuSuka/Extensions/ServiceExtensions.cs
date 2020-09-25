7289ea34d5bdf44db55513aec9d5e3ad56414b12
lamboktulus1379 <lamboktulus1379@gmail.com>
2020-09-25 07:16:19 +0700

    Extension Methods and CORS Configuration

---
+++ AkuSuka/Extensions/ServiceExtensions.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+
+namespace AkuSuka.Extensions
+{
+    public class ServiceExtensions
+    {
+        public static void ConfigureCors(IServiceCollection services)
+        {
+            services.AddCors(options =>
+            {
+                options.AddPolicy("CorsPolicy",
+                    builder => builder.AllowAnyOrigin()
+                    .AllowAnyMethod()
+                    .AllowAnyHeader());
+            });
+        }
+    }
+}
