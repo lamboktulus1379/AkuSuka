a358ff25a7de5423bac93d1caf506b6a16df4d37
lamboktulus1379 <lamboktulus1379@gmail.com>
2020-09-27 01:01:26 +0700

    Added Owner Controller

--- Repository/OwnerRepository.cs
+++ Repository/OwnerRepository.cs
@@ -1,6 +1,9 @@
 ﻿using Contracts;
 using Entities;
 using Entities.Models;
+using System.Collections.Generic;
+using System.Linq;
+
 namespace Repository
 {
     public class OwnerRepository : RepositoryBase<Owner>, IOwnerRepository
@@ -8,6 +11,13 @@ namespace Repository
         public OwnerRepository(RepositoryContext repositoryContext)
             : base(repositoryContext)
         {
+        }
+
+        public IEnumerable<Owner> GetAllOwners()
+        {
+            return FindAll()
+                .OrderBy(ow => ow.Name)
+                .ToList();
         }
     }
 }
